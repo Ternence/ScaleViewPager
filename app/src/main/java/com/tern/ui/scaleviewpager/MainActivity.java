549d06da9e0338868a2d1cb668fa68d89058a897
zhangtengyuan <zhangtengyuan@163.com>
2016-03-15 15:55:13 +0800

    [master] Add scale view pager

---
+++ app/src/main/java/com/tern/ui/scaleviewpager/MainActivity.java
@@ -0,0 +1,86 @@
+package com.tern.ui.scaleviewpager;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private ViewPagerAdapter mPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initView();
+        initData();
+
+    }
+
+    private void initView() {
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mViewPager.setPageTransformer(true, new ScalePageTransformer());
+
+        mPagerAdapter = new ViewPagerAdapter(this);
+        mViewPager.setAdapter(mPagerAdapter);
+    }
+
+    private void initData() {
+        List<Integer> list = new ArrayList<>();
+        list.add(R.drawable.hill);
+        list.add(R.drawable.hill);
+        list.add(R.drawable.hill);
+        list.add(R.drawable.hill);
+        list.add(R.drawable.hill);
+        list.add(R.drawable.hill);
+        list.add(R.drawable.hill);
+        list.add(R.drawable.hill);
+
+        mViewPager.setOffscreenPageLimit(list.size());
+        mPagerAdapter.addAll(list);
+    }
+
+    public static class ViewPagerAdapter extends RecyclingPagerAdapter {
+
+        private final List<Integer> mList;
+        private final Context mContext;
+
+        public ViewPagerAdapter(Context context) {
+            mList = new ArrayList<>();
+            mContext = context;
+        }
+
+        public void addAll(List<Integer> list) {
+            mList.addAll(list);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup container) {
+            ImageView imageView;
+            if (convertView == null) {
+                imageView = new ImageView(mContext);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+            imageView.setTag(position);
+            imageView.setImageResource(mList.get(position));
+            return imageView;
+        }
+
+        @Override
+        public int getCount() {
+            return mList.size();
+        }
+    }
+
+}
